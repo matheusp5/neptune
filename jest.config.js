0846494a9c41ed009a935bdc6e4c28926fbd2ec9
Matheus Piccoli <mxtheuzchika@gmail.com>
2023-07-28 01:20:52 -0300

    Jest configuration

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+﻿module.exports = {
+  testEnvironment: 'node',
+  rootDir: '.',
+  roots: ['./__tests__'],
+  testMatch: ['**/__tests__/**/*.spec.jsx'],
+  moduleNameMapper: {
+    '@/(.*)': './src/$1',
+  },
+  verbose: true,
+}
