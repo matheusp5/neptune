818e8bd57e172674490a27499eb1e3b5e7ca1fa8
Matheus Piccoli <mxtheuzchika@gmail.com>
2023-07-28 01:49:09 -0300

    NeptuneMail class: sendSimpleMail implementation complete

--- src/NeptuneMail.ts
+++ src/NeptuneMail.ts
@@ -19,7 +19,7 @@ class NeptuneMail {
     if (secureSSL) this.smtp_secure = secureSSL;
   }
 
-  async sendSimpleMail(receivers: string[]) {
+  async sendSimpleMail(receivers: string[], subject: string, content: string) {
     try {
       const channel: SMTPChannel = new SMTPChannel({
         host: this.smtp_server,
@@ -30,8 +30,34 @@ class NeptuneMail {
           pass: this.smtp_authentication.password,
         },
       });
+
+      await channel.connect();
+      await channel.helo({ hostName: this.smtp_server });
+
+      const from: string = `<${this.smtp_authentication.email}>`;
+      const to = receivers.map((rcvr: string) => `<${rcvr}>`).join(', ');
+      const headers = {
+        From: from,
+        To: to,
+        Subject: subject,
+      };
+
+      const emailData = Object.entries(headers)
+        .map(([key, value]) => `${key}: ${value}`)
+        .join('\r\n');
+
+      await channel.mail(from);
+
+      for (const receiver of receivers) {
+        await channel.rcpt(`<${receiver}>`);
+      }
+
+      await channel.data(emailData + '\r\n\r\n' + content + '\r\n');
+      await channel.quit();
+
+      console.log('E-mail sent with success!');
     } catch (e) {
-      console.error(`An error ocurred during the email send. ${e}`);
+      console.error(`An error ocurred during the e-mail send. ${e}`);
     }
   }
 }
