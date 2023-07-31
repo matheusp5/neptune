faf03b0b46d97e11cab66fb93c0f5cfeb3cb0b4b
Matheus Piccoli <mxtheuzchika@gmail.com>
2023-07-31 19:06:58 -0300

    QueueBuilder new class

---
+++ src/utils/QueueBuilder.ts
@@ -0,0 +1,25 @@
+﻿class QueueBuilder<T> {
+  private queue: T[];
+
+  constructor() {
+    this.queue = [];
+  }
+
+  enqueue(item: T) {
+    this.queue.push(item);
+  }
+
+  dequeue(): T | undefined {
+    return this.queue.shift();
+  }
+
+  isEmpty(): boolean {
+    return this.queue.length === 0;
+  }
+
+  size(): number {
+    return this.queue.length;
+  }
+}
+
+export default QueueBuilder;
